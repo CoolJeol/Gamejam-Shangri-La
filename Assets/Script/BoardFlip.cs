217aa9d0e51bd467dd2a1f494d98c0633bd1e19a
Snor <jesper.GC.larsson@gmail.com>
2026-01-13 14:16:16 +0100

    Nya modeller och prefabs

--- Assets/Script/BoardFlip.cs
+++ Assets/Script/BoardFlip.cs
@@ -1,16 +1,60 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class BoardFlip : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public List<Transform> Tiles;
+    private const float TIME = 0.1f;
+
+    private void Awake()
     {
-        
+        LeanTween.init();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Keyboard.current.spaceKey.wasPressedThisFrame)
+        {
+            StartCoroutine(StartBoardFlip());
+        }
+
+        IEnumerator StartBoardFlip()
+        {
+            DoAnimation(Tiles[0]);
+            
+            yield return new WaitForSeconds(TIME);
+            
+            DoAnimation(Tiles[3]);
+            DoAnimation(Tiles[1]);
+            
+            yield return new WaitForSeconds(TIME);
+            
+            DoAnimation(Tiles[2]);
+            DoAnimation(Tiles[4]);
+            DoAnimation(Tiles[6]);
+            
+            yield return new WaitForSeconds(TIME);
+            
+            DoAnimation(Tiles[5]);
+            DoAnimation(Tiles[7]);
+            
+            yield return new WaitForSeconds(TIME);
+            
+            DoAnimation(Tiles[8]);
+        }
+    }
+
+    void DoAnimation(Transform trans)
+    {
+        trans.LeanMoveLocalY(0.6f, TIME).setOnComplete(()=> ResetYPosition(trans));
+        trans.LeanRotateAround(new Vector3(1, 0, -1), 180f, TIME * 2f);
+    }
+
+    void ResetYPosition(Transform trans)
+    {
+        trans.LeanMoveLocalY(0, TIME);
     }
 }
