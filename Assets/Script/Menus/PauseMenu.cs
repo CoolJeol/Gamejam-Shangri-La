a4d1c85bb02f5c79ccd1f480a28704274dfdac01
CoolJeol <joel.bfahlberg@gmail.com>
2026-01-14 14:33:28 +0100

    Pause Menu

---
+++ Assets/Script/Menus/PauseMenu.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject Container;
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape)) {
+            Container.SetActive(true);
+            Time.timeScale = 0;
+        }
+    }
+
+    public void resumeButton()
+    {
+        Container.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    public void menuButton()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("");
+    }
+}
